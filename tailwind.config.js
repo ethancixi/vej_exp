4669da8840a0a45a8b6bbe030b1a7fdd842d8e9d
ethancixi <ethan.cixi@gmail.com>
2022-10-05 15:01:22 +0200

    daisyui and carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,26 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {
       fontFamily: {
-        'Tangerine': ['Tangerine', 'serif'],
-        'Cormorant' : ['Cormorant', 'serif']
+        Tangerine: ["Tangerine", "serif"],
+        Cormorant: ["Cormorant", "serif"],
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: false,
+    base: false,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+  },
+};
